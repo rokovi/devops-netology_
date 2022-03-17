2d2ef47d3bb15fba6b975388d02f64950584e8ae
Konstantin Rostovtsev <k.rostovtcev@ya.ru>
2022-03-17 23:29:25 +0300

    [04-script-01-bash]: check

---
+++ 04-script-01-bash/hook.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+pattern="^\[[0-9]{2}-[a-z]{0,8}-[0-9]{2}-[a-z]{0,8}\]:\s.*"
+check=$(echo "$1" | grep -Eoh "$pattern")
+
+if [[ $? == 0 ]] && [[ ${#check} -le 30 ]]
+then
+   echo "Commit message policy is OK"
+    exit 0
+  else
+   echo "Commit message polisy is violated. Example: \"[01-test-01-test]: some text\" and no more then 30 chars."
+    exit 1
+fi
